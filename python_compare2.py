f1f9c3ba646a3534b8321031ae1fc8c84a213bc1
TCantor02 <sneakyninjatx@gmail.com>
2018-06-06 13:00:03 -0500

    modified python compare, second edition

---
+++ python_compare2.py
@@ -0,0 +1,18 @@
+#!/usr/bin python
+
+x =raw_input("Enter first integer")
+
+y = raw_input("Enter second integer")
+
+if x > y:
+    print x, "is the maximum" 
+    print y, "is the minimum"
+    print "if branch"
+
+else: 
+    print y, "is the minimum"
+    print x, "is the minimum"
+    print "else branch"
+
+int(y) - int(x)
+print int(y) - int(x), "is the difference" 
