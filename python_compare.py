e58dd5f04315cef93d2b68e59732030815e31b58
TCantor02 <sneakyninjatx@gmail.com>
2018-06-06 11:08:22 -0500

    my first python script

--- python_compare.py
+++ python_compare.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
 if x == 5: 
-...     print x, 'is equal to 5' 
-...     print 'if branch'
-... else: 
-...     print x, 'is not equal to 5' 
-...     print 'else branch' 
+    print x, 'is equal to 5' 
+    print 'if branch'
+ else: 
+     print x, 'is not equal to 5' 
+    print 'else branch' 
